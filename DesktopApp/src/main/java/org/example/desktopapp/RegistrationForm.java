d2cce378cb62200f49aefdffe57ad60b0645e19d
оман рокопов <roma90657@gmail.com>
2024-02-16 23:54:40 +0300

    почти полностью готовая форма регистрации

--- DesktopApp/src/main/java/org/example/desktopapp/RegistrationForm.java
+++ DesktopApp/src/main/java/org/example/desktopapp/RegistrationForm.java
@@ -1,9 +1,12 @@
 package org.example.desktopapp;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.css.PseudoClass;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
@@ -13,14 +16,16 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.logging.*;
 
+
 public class RegistrationForm extends Application {
-    private static final String SERVER_URL = "http://5.35.96.143:5000/create_user";
+    private static final String SERVER_URL = "http://192.168.0.13:5000/user";
     public static void main(String[] args) {
         launch(args);
     }
@@ -28,7 +33,7 @@ public class RegistrationForm extends Application {
     @Override
     public void start(Stage primaryStage) {
 
-            Logger logger = Logger.getLogger(RegistrationForm.class.getName());
+        Logger logger = Logger.getLogger(RegistrationForm.class.getName());
 
         primaryStage.setTitle("Регистрация пользователя");
 
@@ -57,7 +62,7 @@ public class RegistrationForm extends Application {
 
         Button registerButton = new Button("Зарегистрироваться");
 
-        Text loginErrorText = new Text("Некорректный формат почты");
+        Text loginErrorText = new Text("");
         loginErrorText.setFill(Color.RED);
         loginErrorText.setVisible(false);
 
@@ -65,11 +70,11 @@ public class RegistrationForm extends Application {
         emailErrorText.setFill(Color.RED);
         emailErrorText.setVisible(false);
 
-        Text passwordErrorText = new Text("Некорректный формат почты");
+        Text passwordErrorText = new Text("");
         passwordErrorText.setFill(Color.RED);
         passwordErrorText.setVisible(false);
 
-        Text password2ErrorText = new Text("Некорректный формат почты");
+        Text password2ErrorText = new Text("Пароли не совпадают");
         password2ErrorText.setFill(Color.RED);
         password2ErrorText.setVisible(false);
 
@@ -97,31 +102,86 @@ public class RegistrationForm extends Application {
         primaryStage.setFullScreen(false);
         primaryStage.setResizable(false);
 
-        layout.setOnMouseClicked(event -> layout.requestFocus());
+        layout.setOnMouseReleased(event -> layout.requestFocus());
 
         registerButton.setOnAction(e -> {
             logger.log(Level.INFO, "Кнопка регистрации нажата");
             // Обработка нажатия кнопки регистрации
+
             String login = loginField.getText();
             String email = emailField.getText();
             String password = passwordField.getText();
             String password2 = password2Field.getText();
 
-            if (!email.isEmpty() && !login.isEmpty() && !password.isEmpty() && !password2.isEmpty())
+            if (!email.isEmpty() && !login.isEmpty() && !password.isEmpty() && !password2.isEmpty()) {
                 if (!isValidEmail(email)) {
                     emailErrorText.setVisible(true);
                     emailField.pseudoClassStateChanged(PseudoClass.getPseudoClass("invalid"), true);
-                } else {
-                    boolean registrationSuccessful = sendRegistrationDetails(login, email, password);
 
-                    if (registrationSuccessful) {
-                        logger.log(Level.INFO, "User with login: " + login
-                                                    + ", email: " + email + "registered successfully");
-                    } else {
-                        logger.log(Level.WARNING, "User with login: " + login
-                                + ", email: " + email + "registered invalid");
-                    }
+                    return;
                 }
+
+                String passwordValidationResult = validatePassword(password);
+
+                if (passwordValidationResult != null) {
+                    passwordErrorText.setText(passwordValidationResult);
+                    passwordErrorText.setVisible(true);
+                    passwordField.pseudoClassStateChanged(PseudoClass.getPseudoClass("invalid"), true);
+
+                    return;
+                }
+
+                if (!password.equals(password2)) {
+                    password2ErrorText.setVisible(true);
+                    password2Field.pseudoClassStateChanged(PseudoClass.getPseudoClass("invalid"), true);
+
+                    return;
+                }
+
+                String registrationResponse = sendRegistrationDetails(login, email, password);
+
+                if (registrationResponse != null) {
+                    if (registrationResponse.contains("User created successfully")) {
+                        logger.log(Level.INFO, "Пользователь с логином: " + login
+                                + ", адресом электронной почты: " + email + " успешно зарегистрирован");
+
+                        registerButton.getScene().getWindow().hide();
+                        FXMLLoader loader = new FXMLLoader();
+                        loader.setLocation(getClass().getResource("/org/example/desktopapp/hello-view.fxml"));
+
+                        try {
+                            loader.load();
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+
+                        Parent root = loader.getRoot();
+                        Stage stage = new Stage();
+                        stage.setScene(new Scene(root));
+                        stage.showAndWait();
+
+                    } else if (registrationResponse.contains("There is already such a login")) {
+                        logger.log(Level.INFO, "Такой логин уже существует");
+
+                        loginErrorText.setText("Такой логин уже существует");
+                        loginErrorText.setVisible(true);
+                        loginField.pseudoClassStateChanged(PseudoClass.getPseudoClass("invalid"), true);
+                    } else if (registrationResponse.contains("There is already such an email")) {
+                        logger.log(Level.INFO, "Такая почта уже существует");
+
+                        emailErrorText.setText("Такой адрес электронной почты уже используется");
+                        emailErrorText.setVisible(true);
+                        emailField.pseudoClassStateChanged(PseudoClass.getPseudoClass("invalid"), true);
+                    } else {
+                        logger.log(Level.WARNING, "Пользователь с логином: " + login
+                                + ", адресом электронной почты: " + email + " Сервер выдал ошибку");
+
+                    }
+                } else {
+                    // Обработка случая, когда ответ от сервера null или произошла ошибка
+                    logger.log(Level.WARNING, "Ошибка обработки ответа от сервера");
+                }
+            }
         });
 
         emailField.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -137,6 +197,7 @@ public class RegistrationForm extends Application {
                 loginField.setText(oldValue);
             }
             loginErrorText.setVisible(false);
+            loginField.pseudoClassStateChanged(PseudoClass.getPseudoClass("invalid"), false);
         });
 
         passwordField.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -144,6 +205,7 @@ public class RegistrationForm extends Application {
                 passwordField.setText(oldValue);
             }
             passwordErrorText.setVisible(false);
+            passwordField.pseudoClassStateChanged(PseudoClass.getPseudoClass("invalid"), false);
         });
 
         passwordField.textProperty().addListener((observable, oldValue, newValue) -> {
@@ -151,6 +213,7 @@ public class RegistrationForm extends Application {
                 passwordField.setText(oldValue);
             }
             password2ErrorText.setVisible(false);
+            password2Field.pseudoClassStateChanged(PseudoClass.getPseudoClass("invalid"), false);
         });
 
         registerButton.setOnMousePressed(e -> registerButton.setStyle("-fx-background-color: #5E35B1; " +
@@ -160,8 +223,6 @@ public class RegistrationForm extends Application {
 
 
         primaryStage.show();
-
-
     }
 
     private VBox createCenterPane(TextField loginField, Text loginErrorText,
@@ -198,7 +259,7 @@ public class RegistrationForm extends Application {
         return bottomPane;
     }
 
-    private boolean sendRegistrationDetails(String login, String email, String password) {
+    private String sendRegistrationDetails(String login, String email, String password) {
         try {
             URL url = new URL(SERVER_URL);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -208,34 +269,81 @@ public class RegistrationForm extends Application {
             connection.setDoOutput(true);
 
             String jsonInputString = "{"
-                    + "\"login\":\"" + login + "\","
+                    + "\"username\":\"" + login + "\","
                     + "\"email\":\"" + email + "\","
                     + "\"password\":\"" + password + "\""
                     + "}";
+
             try (OutputStream os = connection.getOutputStream()) {
                 byte[] input = jsonInputString.getBytes("utf-8");
                 os.write(input, 0, input.length);
             }
 
-            try (BufferedReader br = new BufferedReader(
-                    new InputStreamReader(connection.getInputStream(), "utf-8"))) {
-                StringBuilder response = new StringBuilder();
-                String responseLine = null;
-                while ((responseLine = br.readLine()) != null) {
-                    response.append(responseLine.trim());
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK || responseCode == HttpURLConnection.HTTP_CREATED) {
+                try (BufferedReader br = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+                    StringBuilder response = new StringBuilder();
+                    String responseLine;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+                    return response.toString();
                 }
-
-                return response.toString().equals("success");
+            } else {
+                try (BufferedReader br = new BufferedReader(
+                        new InputStreamReader(connection.getErrorStream(), "utf-8"))) {
+                    StringBuilder response = new StringBuilder();
+                    String responseLine;
+                    while ((responseLine = br.readLine()) != null) {
+                        response.append(responseLine.trim());
+                    }
+                    return response.toString();
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
-            return false;
+            return null;
         }
     }
 
     private boolean isValidEmail(String email) {
         return email.matches("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}");
     }
+
+    private String validatePassword(String password) {
+        // Минимальная длина пароля
+        int minLength = 8;
+
+        // Проверка наличия строчных букв
+        boolean containsLowerCase = password.matches(".*[a-z].*");
+
+        // Проверка наличия прописных букв
+        boolean containsUpperCase = password.matches(".*[A-Z].*");
+
+        // Проверка наличия цифр
+        boolean containsDigit = password.matches(".*\\d.*");
+
+        // Проверка наличия специальных символов
+        boolean containsSpecialChar = password.matches(".*[!@#$%^&*()-_=+{};:,<.>/?\\[\\]\\\\].*");
+
+        // Проверка минимальной длины и наличия всех критериев
+        if (password.length() < minLength) {
+            return "Пароль должен содержать не менее " + minLength + " символов";
+        } else if (!containsLowerCase) {
+            return "Пароль должен содержать строчные буквы";
+        } else if (!containsUpperCase) {
+            return "Пароль должен содержать заглавные буквы";
+        } else if (!containsDigit) {
+            return "Пароль должен содержать цифры";
+        } else if (!containsSpecialChar) {
+            return "Пароль должен содержать специальные символы";
+        }
+
+        // Пароль прошел все проверки
+        return null;
+    }
+
 }
 
 
