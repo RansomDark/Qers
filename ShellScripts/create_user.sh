4ffebc7bd23f8cf12d9975721e667894ff92f958
Roman Prokopov <roma90657@gmail.com>
2025-03-30 19:31:55 +0300

    Удаление того, что не пригодилось

--- ShellScripts/create_user.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Запрашиваем у пользователя ввод пароля и логина
-read -p "Введите логин: " username
-read -p "Введите пароль: " password
-
-# Отправляем запрос на создание пользователя
-curl -X POST -H "Content-Type: application/json" -d "{\"password\": \"$password\", \"username\": \"$username\"}" http://localhost:5000/user
